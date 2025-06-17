987aabc2ce2501378bac790a52bf5fdbe3c32a3d
Jonah-Schwartz521 <josc6888@colorado.edu>
2025-06-16 19:23:48 -0500

    upload sql files

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,19 @@
+
+-- Question: What are the most in-demand skills for a data analyst?
+
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact 
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id  
+WHERE 
+    job_title_short = 'Data Analyst' AND 
+    job_location = 'Chicago, IL' AND 
+    job_work_from_home = False
+GROUP BY
+    skills
+ORDER BY 
+    demand_count DESC
+lIMIT 5
