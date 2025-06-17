e6054074a3f68470ef6a5d7124f4b91b6b1f0af3
Jonah-Schwartz521 <josc6888@colorado.edu>
2025-06-16 19:22:49 -0500

    upload sql files

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,6 @@
+/*
+Question: What are the top paying business analyst jobs?
+- Identify the top 10 highest paying Business Analyst Jobs availible
+- Focuses on job postings with specified salaries (remove nulls)
+- Why? Highlight the top paying oppertunities for Business Analysts
+*/ 
